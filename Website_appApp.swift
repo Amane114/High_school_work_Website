67f2ec90095b5a057f51d54de826e74d2d8e7485
Amane114 <ojoubntnn@hotmail.com>
2025-03-07 11:48:29 -0800

    Add files via upload

---
+++ Website_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Website_appApp.swift
+//  Website app
+//
+//  Created by APCSP on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Website_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Welcome to My Website! Let's go ahead");
+});
